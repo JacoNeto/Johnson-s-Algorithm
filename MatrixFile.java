a32216413e99e2101e32b40d87023d307ec4963b
JacoNeto <jaconeto8@gmail.com>
2022-03-29 08:09:17 -0300

    directory change

--- MatrixFile.java
+++ MatrixFile.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 public class MatrixFile {
 	public static int[][] Read() throws FileNotFoundException {
-		Scanner sc = new Scanner(new BufferedReader(new FileReader("src/adjacente_original.txt")));
+		Scanner sc = new Scanner(new BufferedReader(new FileReader("src/assets/adjacente_original.txt")));
 
 		int rows = 1;
 		int columns = 0;
@@ -23,7 +23,7 @@ public class MatrixFile {
 		int[][] myArray = new int[rows][columns];
 
 		sc.close();
-		sc = new Scanner(new BufferedReader(new FileReader("src/adjacente_original.txt")));
+		sc = new Scanner(new BufferedReader(new FileReader("src/assets/adjacente_original.txt")));
 
 		while (sc.hasNextLine()) {
 			for (int i = 0; i < myArray.length; i++) {
@@ -38,7 +38,7 @@ public class MatrixFile {
 	}
 	
 	public static void Write(String str) throws IOException {
-		BufferedWriter writer = new BufferedWriter(new FileWriter("src/adjacente_resultante.txt"));
+		BufferedWriter writer = new BufferedWriter(new FileWriter("src/assets/adjacente_resultante.txt"));
 	    writer.write(str);
 	    
 	    writer.close();
