ad863e83a75ce4cf8b7a1fbf8b812f34cc7399d1
JacoNeto <jaconeto8@gmail.com>
2022-03-30 14:18:17 -0300

    update

--- JohnsonAlgorithm.java
+++ JohnsonAlgorithm.java
@@ -4,31 +4,40 @@ public class JohnsonAlgorithm {
 
 	// procedimento de calculo do algoritmo
 	public static void calc(int[][] matrizCusto) {
-		
-		int vertices; // numero de vertices da matriz de custo
-		int arestas; // numero de arestas da matriz de custo
-		
+
+		int vertices = 0; // numero de vertices da matriz de custo
+		int arestas = 0; // numero de arestas da matriz de custo
+
 		String str = ""; // saida do programa
 
 		int[][] matrizAdjacente;
-		
+
 		vertices = matrizCusto[0].length;
 		arestas = matrizCusto.length;
 		
+		// obtém o número de vértices
+		for (int i = 0; i < arestas; i++) {
+			vertices = Utils.valorMaximo(matrizCusto[i][0], vertices);
+		}
+		for (int i = 0; i < arestas; i++) {
+			vertices = Utils.valorMaximo(matrizCusto[i][1], vertices);
+		}
+
 		// a matriz adjacente é uma matriz quadrática com o número de vértices
 		// da matriz de custo
-		matrizAdjacente = new int[vertices][vertices]; 
-		
+
+		matrizAdjacente = new int[vertices][vertices];
+
 		// preenche a matriz adjacente com os valores da matriz de custo
 		for (int k = 0; k < arestas; k++) {
 			matrizAdjacente[matrizCusto[k][0] - 1][matrizCusto[k][1] - 1] = matrizCusto[k][2];
 		}
-		
-		
+
 		for (int i = 0; i < vertices; i++) {
 			for (int j = 0; j < vertices; j++) {
 				// Se não há aresta, usa valor maximo/infinito
 				if (matrizAdjacente[i][j] == 0 && i != j) {
+					System.out.println("slmfnfdkjjgn " + i + " " + j);
 					matrizAdjacente[i][j] = Integer.MAX_VALUE;
 				}
 			}
@@ -54,7 +63,7 @@ public class JohnsonAlgorithm {
 			}
 			str += "\n";
 		}
-		
+
 		try {
 			MatrixFile.Write(str);
 		} catch (IOException e) {
--- Utils.java
+++ Utils.java
@@ -1,8 +1,13 @@
 
 public class Utils {
-	
+
 	// retorna o valor minimo entre dois numeros
 	public static int valorMinimo(int n1, int n2) {
-		return (n1<n2) ? n1 : n2;
+		return (n1 < n2) ? n1 : n2;
+	}
+
+	// retorna o valor maximo entre dois numeros
+	public static int valorMaximo(int n1, int n2) {
+		return (n1 > n2) ? n1 : n2;
 	}
 }
